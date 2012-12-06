92198f59285a51ed6d259ef888b97ecb9baaf59d
Amit Acharya <amitach@gmail.com>
2012-12-07 00:46:02 +0530

    Added first test for product model, unit test

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "product attributes must not be empty" do
+     product = Product.new
+     product.title = "Binod"
+     product.description = "time pass"
+     product.price = 100.00
+     product.image_url = "rails.png"
+     product.save
+     assert product.valid?
+     assert product.errors[:title].blank?
+     assert product.errors[:description].blank?
+     assert product.errors[:price].blank?
+     assert product.errors[:image_url].blank?
+   end
 end
